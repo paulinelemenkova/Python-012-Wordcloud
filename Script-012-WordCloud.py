54792b0804cbfac13a6598b3e236779aab036ffe
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2019-06-12 20:41:56 +0800

    Add files via upload

--- Script-012-WordCloud.py
+++ Script-012-WordCloud.py
@@ -1,49 +1,25 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[41]:
-
-
+#
 from wordcloud import WordCloud
 from os import path
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 import os
-
+#
 d = path.dirname(__file__) if "__file__" in locals() else os.getcwd()
- 
+#
 # Create a list of word
-#text=("text")
 text = open('Wordcloud-Mariana.txt').read()
 fish_mask = np.array(Image.open(path.join(d, "fish-1.jpg")))
-
-# Create the wordcloud object
-#wordcloud = WordCloud(width=480, height=480, margin=0).generate(text)
+#
+# Create wordcloud object
 wordcloud = WordCloud(width=480, height=480, margin=0, background_color="white", mask=fish_mask,
                      contour_width=1, contour_color='grey', colormap='viridis').generate(text)
- 
-# Display the generated image:
+#
+# Display image:
 plt.imshow(wordcloud, interpolation='bilinear', cmap=plt.cm.gray)
 plt.axis("off")
 plt.margins(x=0, y=0)
 plt.show()
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
