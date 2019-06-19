cb58ce92284f8b264dfbce0232551f17e5c465a3
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2019-06-19 20:52:39 +0800

    Add files via upload

--- Script-012-WordCloudOcean.py
+++ Script-012-WordCloudOcean.py
@@ -1,25 +1,32 @@
 #!/usr/bin/env python
 # coding: utf-8
-#
-from wordcloud import WordCloud
+import os
 from os import path
-from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
-import os
-#
+from wordcloud import WordCloud
+from PIL import Image
+
 d = path.dirname(__file__) if "__file__" in locals() else os.getcwd()
-#
-# Create a list of word
+
+# create a list of word
 text = open('Wordcloud-Ocean.txt').read()
-fish_mask = np.array(Image.open(path.join(d, "fish-2.jpg")))
-#
-# Create wordcloud object
-wordcloud = WordCloud(width=480, height=480, margin=0, background_color="white",
-                      mask=fish_mask, contour_width=1, contour_color='grey', colormap='cool').generate(text)
-#
-# Display image:
+fish_mask = np.array(Image.open(path.join(d, "fish-2.jpg")
+                                )
+                     )
+
+# create wordcloud object
+wordcloud = WordCloud(width=480, height=480, margin=0,
+                      background_color="white",
+                      mask=fish_mask, contour_width=1,
+                      contour_color='grey', colormap='cool'
+                      ).generate(text)
+
+# plotting
 plt.imshow(wordcloud, interpolation='bilinear', cmap=plt.cm.gray)
 plt.axis("off")
 plt.margins(x=0, y=0)
+
+# visualizing and saving
+plt.savefig('plot_WordCo.png', dpi=300)
 plt.show()
